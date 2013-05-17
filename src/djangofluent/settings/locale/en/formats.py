42fcba6ebe799bc74dd59a456732c6275d820559
Diederik van der Boor <vdboor@edoburu.nl>
2013-05-17 14:00:40 +0200

    Add FOOTER_DATE_FORMAT for english locale

---
+++ src/djangofluent/settings/locale/en/formats.py
@@ -0,0 +1,15 @@
+"""
+Custom locale settings for NL.
+
+This file is read because of the following setting:
+
+    FORMAT_MODULE_PATH = 'djangofluent.settings.locale'
+
+"""
+
+DATETIME_FORMAT = r'F j, Y \a\t H:i'      # '20 januari 2009 at 15:23'
+#DATETIME_FORMAT = r'j F Y, H:i'          # '20 januari 2009, 15:23'
+
+ENTRY_DATETIME_FORMAT = r'l j F Y, H:i'
+
+FOOTER_DATE_FORMAT = r'j F Y'
