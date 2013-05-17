e09aff7ea90daeb3048d3cdfb7e5a70eaaeb6a09
Diederik van der Boor <vdboor@edoburu.nl>
2013-05-17 02:08:33 +0200

    Cleanup urls.py using latest project-template version

--- src/djangofluent/urls.py
+++ src/djangofluent/urls.py
@@ -1,10 +1,10 @@
 from django.conf.urls import *
 from django.conf import settings
 from django.contrib import admin
-from filebrowser.sites import site as fbsite
+from filebrowser.sites import site as fb_site
 from fluent_blogs.sitemaps import EntrySitemap, CategoryArchiveSitemap, AuthorArchiveSitemap, TagArchiveSitemap
 from fluent_pages.sitemaps import PageSitemap
-from frontend.views import TextFileView
+from frontend.views import TextFileView, Http500View
 
 admin.autodiscover()
 
@@ -17,26 +17,16 @@ sitemaps = {
 }
 
 urlpatterns = patterns('',
+    # Django admin
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^admin/filebrowser/', include(fbsite.urls)),
-    url(r'^admin/util/taggit_autocomplete_modified/', include('taggit_autocomplete_modified.urls')),
-    url(r'^admin/util/tinymce/', include('tinymce.urls')),
+    url(r'^admin/filebrowser/', include(fb_site.urls)),
+    url(r'^admin/util/tags/', include('taggit_autocomplete_modified.urls')),
     url(r'^admin/util/tools/', include('admin_tools.urls')),
-    url(r'^blog/comments/', include('fluent_comments.urls')),
-)
+    url(r'^admin/util/tinymce/', include('tinymce.urls')),
 
-if settings.DEBUG:
-    urlpatterns += patterns('',
-        (r'^media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT, 'show_indexes': True}),
-    )
-
-def view500(request):
-    raise NotImplementedError("500 error page test")
-
-# Include all remaining pages URLs from the CMS
-urlpatterns += patterns('',
-    url(r'^500test/$', view=view500),
+    # Test pages
+    url(r'^500test/$', view=Http500View.as_view()),
     url(r'^403/$', 'django.views.defaults.permission_denied'),
     url(r'^404/$', 'django.views.defaults.page_not_found'),
     url(r'^500/$', 'django.views.defaults.server_error'),
@@ -46,5 +36,11 @@ urlpatterns += patterns('',
     url(r'^robots.txt$', TextFileView.as_view(content_type='text/plain', template_name='robots.txt')),
 
     # CMS modules
+    url(r'^blog/comments/', include('fluent_comments.urls')),
     url(r'', include('fluent_pages.urls')),
 )
+
+if settings.DEBUG:
+    urlpatterns.insert(0,
+        url(r'^media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT, 'show_indexes': True})
+    )
