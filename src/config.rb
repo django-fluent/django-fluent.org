ba4f728a001ff92443697d3188e985b6df77c048
Diederik van der Boor <vdboor@edoburu.nl>
2014-03-31 23:40:49 +0200

    Add Gemfile for better Compass support

--- src/config.rb
+++ src/config.rb
@@ -4,6 +4,7 @@ http_path = "/"
 css_dir = "frontend/static/frontend/css"
 sass_dir = "frontend/sass"
 images_dir = "frontend/static/frontend/img"
+fonts_dir = "frontend/static/frontend/fonts"
 javascripts_dir = "frontend/static/frontend/js"
 
 output_style = :expanded # or :nested or :compact or :compressed
@@ -14,5 +15,5 @@ sass_options = {
     :debug_info => false
 }
 
-# django-compressor already adds file hashes.
+# django-compressor already adds file hashes, compass doesn't have to.
 asset_cache_buster :none
