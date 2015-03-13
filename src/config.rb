271385673f6529fefc247e4239c92ed5b4e959c2
Diederik van der Boor <vdboor@edoburu.nl>
2015-03-13 13:29:29 +0100

    frontend: recompile with latest Sass

--- src/config.rb
+++ src/config.rb
@@ -10,10 +10,9 @@ javascripts_dir = "frontend/static/frontend/js"
 output_style = :expanded # or :nested or :compact or :compressed
 relative_assets = true
 line_comments = false
+sourcemap = false
 
-sass_options = {
-    :debug_info => false
-}
+sass_options = {}
 
 # django-compressor already adds file hashes, compass doesn't have to.
 asset_cache_buster :none
