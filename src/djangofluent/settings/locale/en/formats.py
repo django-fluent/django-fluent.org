bfd80f9b6a689264370f4e144d412b0886f0f67e
Diederik van der Boor <vdboor@edoburu.nl>
2015-12-31 13:04:45 +0100

    Fix date formatting in sidebar
    Django 1.8.7 patched the use of {{ ..|date:"SETTING_NAME" }}

--- src/djangofluent/settings/locale/en/formats.py
+++ src/djangofluent/settings/locale/en/formats.py
@@ -9,7 +9,3 @@ This file is read because of the following setting:
 
 DATETIME_FORMAT = r'F j, Y \a\t H:i'      # '20 januari 2009 at 15:23'
 #DATETIME_FORMAT = r'j F Y, H:i'          # '20 januari 2009, 15:23'
-
-ENTRY_DATETIME_FORMAT = r'l j F Y, H:i'
-
-FOOTER_DATE_FORMAT = r'j F Y'
