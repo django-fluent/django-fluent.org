56ad612fb9f4c3154dff10231a3753b048c15883
Diederik van der Boor <vdboor@edoburu.nl>
2016-04-13 16:13:20 +0200

    Replace named URLs with imported python packages (1.9 compat)

--- src/djangofluent/urls.py
+++ src/djangofluent/urls.py
@@ -1,14 +1,23 @@
-from django.conf.urls import url, include
+import admin_tools.urls
+import django.contrib.sitemaps.views
+import django.views.defaults
+import django.views.static
+import fluent_comments.urls
+import fluent_pages.urls
+import ping.urls
+import taggit_autosuggest.urls
+import tinymce.urls
+
 from django.conf import settings
+from django.conf.urls import url, include
 from django.contrib import admin
+from django.utils.functional import curry
 from filebrowser.sites import site as fb_site
 from fluent_blogs.sitemaps import EntrySitemap, CategoryArchiveSitemap, AuthorArchiveSitemap, TagArchiveSitemap
 from fluent_pages.sitemaps import PageSitemap
 from fluent_pages.views import RobotsTxtView
 from os import path
 from frontend.views import Http500View
-
-admin.autodiscover()
 
 sitemaps = {
     'pages': PageSitemap,
@@ -22,34 +31,35 @@ urlpatterns = [
     # Django admin
     url(r'^admin/', include(admin.site.urls)),
     url(r'^admin/filebrowser/', include(fb_site.urls)),
-    url(r'^admin/util/tags/', include('taggit_autosuggest.urls')),
-    url(r'^admin/util/tools/', include('admin_tools.urls')),
-    url(r'^admin/util/tinymce/', include('tinymce.urls')),
+    url(r'^admin/util/tags/', include(taggit_autosuggest.urls)),
+    url(r'^admin/util/tinymce/', include(tinymce.urls)),
+    url(r'^admin/util/tools/', include(admin_tools.urls)),
 
     # Test pages
     url(r'^500test/$', view=Http500View.as_view()),
-    url(r'^403/$', 'django.views.defaults.permission_denied'),
-    url(r'^404/$', 'django.views.defaults.page_not_found'),
-    url(r'^500/$', 'django.views.defaults.server_error'),
+    url(r'^400/$', curry(django.views.defaults.bad_request, exception=None)),
+    url(r'^403/$', curry(django.views.defaults.permission_denied, exception=None)),
+    url(r'^404/$', curry(django.views.defaults.page_not_found, exception=None)),
+    url(r'^500/$', django.views.defaults.server_error),
 
     # SEO API's
-    url(r'^sitemap.xml$', 'django.contrib.sitemaps.views.sitemap', {'sitemaps': sitemaps}),
+    url(r'^sitemap.xml$', django.contrib.sitemaps.views.sitemap, {'sitemaps': sitemaps}),
     url(r'^robots.txt$', RobotsTxtView.as_view()),
 
     # Monitoring API's
-    url(r'^api/ping/', include('ping.urls')),
+    url(r'^api/ping/', include(ping.urls)),
 
     # CMS modules
-    url(r'^blog/comments/', include('fluent_comments.urls')),
-    url(r'', include('fluent_pages.urls')),
+    url(r'^blog/comments/', include(fluent_comments.urls)),
+    url(r'', include(fluent_pages.urls)),
 ]
 
 if settings.DEBUG:
     # For runserver, also host the media files
     # Sass files are exported so browsers can open the Sass sources via sourcemaps.
     urlpatterns = [
-        url(r'^media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT, 'show_indexes': True}),
-        url(r'^sass/(?P<path>.*)$', 'django.views.static.serve', {'document_root': path.join(settings.SRC_DIR, 'frontend/sass'), 'show_indexes': True}),
+        url(r'^media/(?P<path>.*)$', django.views.static.serve, {'document_root': settings.MEDIA_ROOT, 'show_indexes': True}),
+        url(r'^sass/(?P<path>.*)$', django.views.static.serve, {'document_root': path.join(settings.SRC_DIR, 'frontend', 'sass'), 'show_indexes': True}),
     ] + urlpatterns
 
     if 'debug_toolbar' in settings.INSTALLED_APPS:
