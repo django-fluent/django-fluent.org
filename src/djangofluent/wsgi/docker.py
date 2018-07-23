a681fc578cdfeb072b9e76a84036a73a237dae1b
Diederik van der Boor <vdboor@edoburu.nl>
2018-07-23 15:38:14 +0200

    Fix wsgiunproxy IP address for new Kubernetes network config

--- src/djangofluent/wsgi/docker.py
+++ src/djangofluent/wsgi/docker.py
@@ -5,4 +5,4 @@ from wsgiunproxy import unproxy
 # Export application object with WSGI middleware
 # Allow X-Forwarded-For from Kubernetes ingress
 application = get_wsgi_application()
-application = unproxy(trusted_proxies=IPNetwork('10.0.0.0/8'))(application)
+application = unproxy(trusted_proxies=IPNetwork('192.168.0.0/16'))(application)
