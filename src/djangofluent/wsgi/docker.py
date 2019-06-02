300341e3fc5d7a357469dd734d54c71e30bc3353
Diederik van der Boor <vdboor@edoburu.nl>
2019-06-03 00:36:42 +0200

    Fix IP detection

--- src/djangofluent/wsgi/docker.py
+++ src/djangofluent/wsgi/docker.py
@@ -1,8 +1,11 @@
+import os
+
 from django.core.wsgi import get_wsgi_application
 from netaddr import IPNetwork
 from wsgiunproxy import unproxy
 
 # Export application object with WSGI middleware
 # Allow X-Forwarded-For from Kubernetes ingress
+trusted_cidr = os.environ.get("ALLOWED_CIDR_NETS", "10.0.0.0/8")
 application = get_wsgi_application()
-application = unproxy(trusted_proxies=IPNetwork('192.168.0.0/16'))(application)
+application = unproxy(trusted_proxies=IPNetwork(trusted_cidr))(application)
